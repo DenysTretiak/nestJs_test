343f67dbe7ab4444fd52e72606a763c01cacc4bd
Denys Tretiak <Desmond745@gmail.com>
2024-01-03 21:40:24 +0200

    add grud operations

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Post, Put } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('/test')
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('/test')
+  postMessage(@Body() body): string {
+    return body;
+  }
+
+  @Put('/test')
+  updateMessage(@Body() body): string {
+    return body;
+  }
+
+  @Delete('/test')
+  deleteMessage(@Body() body): string {
+    return body;
   }
 }
